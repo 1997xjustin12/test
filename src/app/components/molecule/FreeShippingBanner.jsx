703acdcc0307ce25dbf00510995c5836863e49f1
1997xjustin12 <1997justin12@gmail.com>
2025-08-18 15:25:33 +0800

    update: text, contact and link

--- src/app/components/molecule/FreeShippingBanner.jsx
+++ src/app/components/molecule/FreeShippingBanner.jsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
 import { DeliveryTruckSpeedIcon } from "@/app/components/icons/lib";
 export default function FreeShippingBanner() {
+  const contact = "(888) 575-9720";
   return (
     <div className="bg-[#4C4C53] flex items-center justify-center py-[8px] gap-[20px]">
       <div className="text-white text-sm w-[calc(100%-50px)] md:w-[auto] pl-[10px] md:pl-[0px]">
-        Enjoy Free Shipping on All Orders Over $99 – Shop Now and Save!
+        Enjoy Free Shipping on Select Orders. <span><Link className="underline italic" prefetch={false} href={`tel:${contact}`}>Call now for details</Link></span>
       </div>
       <DeliveryTruckSpeedIcon width="28" height="28" color="white" />
     </div>
