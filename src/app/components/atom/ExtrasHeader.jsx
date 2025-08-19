f7c89ab2b982958f5ae99279064e61236da13f6b
1997xjustin12 <1997justin12@gmail.com>
2025-08-19 15:12:40 +0800

    update: links

--- src/app/components/atom/ExtrasHeader.jsx
+++ src/app/components/atom/ExtrasHeader.jsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 import { BASE_URL } from "@/app/lib/helpers";
 function ExtrasHeader() {
     const extrasLinks = [
-        {name: "Promotions", url:"promotions"},
-        {name: "Learning Center", url:""},
-        {name: "Professional Program", url:""},
-        {name: "Order Status", url:""},
-        {name: "Why Buy From The Expert In Fire?", url:""},
+        {name: "Promotions", url:`${BASE_URL}/brand/eloquence`},
+        {name: "Learning Center", url:" https://outdoorkitchenoutlet.com/blogs/general-information"},
+        {name: "Professional Program", url:"https://outdoorkitchenoutlet.com/pages/contractor-program"},
+        {name: "Order Status", url:" https://outdoorkitchenoutlet.com/pages/contact-us"},
+        // {name: "Why Buy From The Expert In Fire?", url:""},
     ]
   return (
     <div className="hidden lg:block bg-orange-500 py-[8px] px-[30px] text-white">
@@ -18,7 +18,7 @@ function ExtrasHeader() {
                     key={`extras-links-${item?.url}-${index}`}
                     className="font-light text-xs"
                     prefetch={false}
-                    href={`${BASE_URL}/${item?.url}`}>{item?.name}</Link>)
+                    href={`${item?.url}`}>{item?.name}</Link>)
             }
         </div>
       </div>
