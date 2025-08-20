c4b9dc4659b161af7ec2dbe0590aa4275ada2973
1997xjustin12 <1997justin12@gmail.com>
2025-08-20 18:21:41 +0800

    fix: add condition before render

--- src/app/components/atom/CartOrderSummary.jsx
+++ src/app/components/atom/CartOrderSummary.jsx
@@ -100,9 +100,12 @@ function CartOrderSummary() {
 
   return (
     <div className="mx-auto mt-6 max-w-4xl flex-1 space-y-6 lg:mt-0 lg:w-full">
-      <div className=" border-green-700 p-4 shadow-sm dark:border-gray-700 dark:bg-gray-800 rounded-lg bg-green-300 border font-extrabold sm:p-6 italic">
+      {
+        Number(savings) > 0 && 
+        <div className=" border-green-700 p-4 shadow-sm dark:border-gray-700 dark:bg-gray-800 rounded-lg bg-green-300 border font-extrabold sm:p-6 italic">
           You are saving ${formatPrice(savings)} plus Free Shipping
       </div>
+      }
       <div className="space-y-4 rounded-lg border border-gray-200 bg-white p-4 shadow-sm dark:border-gray-700 dark:bg-gray-800 sm:p-6">
         <p className="text-xl font-semibold text-gray-900 dark:text-white">
           Order summary
