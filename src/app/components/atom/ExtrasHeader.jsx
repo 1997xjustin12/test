54eafc700b50e60397e72648e77a833592140a4f
1997xjustin12 <1997justin12@gmail.com>
2025-08-28 14:09:00 +0800

    partial: eloquence page

--- src/app/components/atom/ExtrasHeader.jsx
+++ src/app/components/atom/ExtrasHeader.jsx
@@ -3,8 +3,10 @@ import Link from "next/link";
 import { BASE_URL } from "@/app/lib/helpers";
 function ExtrasHeader() {
     const extrasLinks = [
-        {name: "Promotions", url:`https://outdoorkitchenoutlet.com/collections/eloquence-1`},
-        {name: "Learning Center", url:" https://outdoorkitchenoutlet.com/blogs/general-information"},
+        // {name: "Promotions", url:`https://outdoorkitchenoutlet.com/collections/eloquence-1`},
+        {name: "Promotions", url:`${BASE_URL}/brand/eloquence`},
+        // {name: "Learning Center", url:" https://outdoorkitchenoutlet.com/blogs/general-information"},
+        {name: "Learning Center", url:`${BASE_URL}/blogs`},
         {name: "Professional Program", url:`${BASE_URL}/professional-program`},
         {name: "Support", url:`${BASE_URL}/contact`},
         // {name: "Why Buy From The Expert In Fire?", url:""},
