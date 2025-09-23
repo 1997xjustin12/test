7804d57fea217170d748ac00ab630e580a257d14
1997xjustin12 <1997justin12@gmail.com>
2025-09-23 18:18:32 +0800

    build fix

--- src/middleware.js
+++ src/middleware.js
@@ -7,9 +7,9 @@ export function middleware(request) {
   const cart = JSON.parse(request.cookies.get("cart")?.value || "[]");
   const isLoggedIn = request.cookies.get("isLoggedIn")?.value === "true";
 
-  if (block_links.includes(pathname)) {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
+  // if (block_links.includes(pathname)) {
+  //   return NextResponse.redirect(new URL("/", request.url));
+  // }
 
   if (pathname === "/checkout" && cart.length === 0) {
     return NextResponse.redirect(new URL("/", request.url));
