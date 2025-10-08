a40d9094ae5d02a3bb0e7e5563d6bb0a3c8056dd
1997xjustin12 <1997justin12@gmail.com>
2025-10-08 10:41:50 +0800

    updated connection domain list and csp

--- next.config.ts
+++ next.config.ts
@@ -11,9 +11,13 @@ const imageDomains = [
 
 // 2. Define all external hosts your application fetches/connects to (client-side)
 const connectDomains = [
-  "https://r2.leadsy.ai", // Required for the new script
   "https://admin.solanabbqgrills.com",
   "https://loyal-sloth-59774.upstash.io",
+  "https://api.iconify.design", // icons
+  "https://r2.leadsy.ai", // Required for the new script
+  "https://wvbknd.leadsy.ai", 
+  "https://api.iconify.design",
+  "https://tag.trovo-tag.com"
   // "http://164.92.65.4", // Assuming 9200 is unnecessary and you need the IP
 ];
 
@@ -27,13 +31,15 @@ module.exports = {
     const CSP = `
       default-src 'self';
       
-      script-src 'self' 'unsafe-eval' 'unsafe-inline' https://r2.leadsy.ai;
+      script-src 'self' 'unsafe-eval' 'unsafe-inline' https://r2.leadsy.ai https://tag.trovo-tag.com;
       
       style-src 'self' 'unsafe-inline';
       
       img-src 'self' data: blob: ${imageDomains.join(" ")}; 
       
       connect-src 'self' ${connectDomains.join(" ")}; 
+
+      frame-src 'self' https://tag.trovo-tag.com; 
     `;
 
     return [
