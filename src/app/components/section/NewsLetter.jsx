03d2e3854ee445602cd9f5a98f4eb7d188bc8285
1997xjustin12 <1997justin12@gmail.com>
2025-10-14 12:44:52 +0800

    add: subscribe and unsubscribe components to /my-account

--- src/app/components/section/NewsLetter.jsx
+++ src/app/components/section/NewsLetter.jsx
@@ -1,4 +1,29 @@
+"use client";
+import { useState } from "react";
+import { subscribe } from "@/app/lib/api";
+import { Eos3DotsLoading } from "@/app/components/icons/lib";
+
 export default function NewsLetter() {
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      await subscribe(email);
+    } catch (err) {
+      console.warn(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setEmail(value);
+  };
+
   return (
     <div className="mt-20 bg-[#f1f1f1] py-[30px]">
       <div className="container mx-auto  gap-[30px] flex flex-col justify-center text-center">
@@ -8,14 +33,32 @@ export default function NewsLetter() {
         </div>
         <div>
           <div className="border inline-block">
-            <input
-              type="text"
-              placeholder="Enter Email Address"
-              className=" text-sm md:text-base p-[15px] rounded-none outline-none focus:border-theme-300 focus:ring-orange-300 bg-[#f1f1f1] ring:2 border-theme-500 border md:w-[500px]"
-            />
-            <button className="py-[15px] px-[30px] text-white bg-theme-500 border border-theme-500 text-sm md:text-base">
-              Join
-            </button>
+            <form onSubmit={handleSubmit}>
+              <input
+                type="email"
+                name="email"
+                required
+                value={email || ""}
+                onChange={handleChange}
+                placeholder="Enter Email Address"
+                className=" text-sm md:text-base p-[15px] bg-white rounded-none outline-none focus:border-theme-300 focus:ring-orange-300 ring:2 border-theme-500 border md:w-[500px]"
+              />
+              <button
+                disabled={loading}
+                className="w-[100px] h-[56px] text-white bg-theme-500 border border-theme-500 text-sm md:text-base relative"
+              >
+                <div
+                  className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ${
+                    loading === true ? "visible" : "invisible"
+                  }`}
+                >
+                  <Eos3DotsLoading width={70} height={70} />
+                </div>
+                <span className={loading === true ? "invisible" : "visible"}>
+                  Join
+                </span>
+              </button>
+            </form>
           </div>
         </div>
       </div>
--- src/app/lib/api.js
+++ src/app/lib/api.js
@@ -1,4 +1,4 @@
-export const sendAbandonedCart= async(cart) => {
+export const sendAbandonedCart = async(cart) => {
   try {
     if (!cart) {
       console.warn("[ABANDONED CART] No valid cart to send");
@@ -14,6 +14,47 @@ export const sendAbandonedCart= async(cart) => {
     });
 
   } catch (err) {
-    // console.error("[ABANDONED CART] API error:", err);
+    console.warn("[ABANDONED CART] API error:", err);
+  }
+}
+
+
+export const subscribe = async(email) => {
+  try {
+    if (!email) {
+      console.warn("[email] Requied field missing.");
+      return;
+    }
+
+    return await fetch("/api/subscribers/subscribe", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({email}),
+    });
+
+  } catch (err) {
+    console.warn("[subscribe] API error:", err);
+  }
+}
+
+export const unsubscribe = async(email) => {
+  try {
+    if (!email) {
+      console.warn("[email] Requied field missing.");
+      return;
+    }
+
+    return await fetch("/api/subscribers/unsubscribe", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({email}),
+    });
+
+  } catch (err) {
+    console.warn("[unsubscribe] API error:", err);
   }
 }
