21943248aae0ee71594a25a6bef1d7edea6f05fd
1997xjustin12 <1997justin12@gmail.com>
2026-06-05 12:25:50 +0800

    theming: applied conditional rendering for forgot password page

--- src/app/(market)/forgot-password/page.jsx
+++ src/app/(market)/forgot-password/page.jsx
@@ -1,38 +1,23 @@
 import React from "react";
 
-import ForgotPassword from "@/app/components/form/ForgotPassword";
 import Link from "next/link";
-import { BASE_URL } from "@/app/lib/helpers";
+import { BASE_URL, ISBBQ } from "@/app/lib/helpers";
 import { STORE_NAME } from "@/app/lib/store_constants";
+
+import NewForgotPasswordPage from "@/app/components/new-design/page/ForgotPassword";
+import BBQForgotPasswordPage from "@/app/components/bbq-design/page/ForgotPassword";
+
 export const metadata = {
   title: `Forgot Password | ${STORE_NAME}`,
 };
 
 function ForgotPasswordPage() {
+  if(ISBBQ){
+    return <BBQForgotPasswordPage />
+  }
+
   return (
-    <div className="container mx-auto">
-      <div className="px-4 py-[50px]">
-        <div className="flex justify-center">
-          <div className="max-w-[400px] flex flex-col items-center">
-            <h2 className="font-extrabold mb-5">Forgot your password?</h2>
-            <p className="mb-5 text-sm font-medium text-neutral-600 text-center">
-              No worries. Enter your email address below and we’ll send you a
-              link to reset your password.
-            </p>
-            <div className="my-[20px] w-full">
-              <ForgotPassword />
-            </div>
-            <Link
-              prefetch={false}
-              href={`${BASE_URL}/login`}
-              className="text-theme-600 hover:underline block text-sm font-bold"
-            >
-              Back to Login
-            </Link>
-          </div>
-        </div>
-      </div>
-    </div>
+    <NewForgotPasswordPage />
   );
 }
 
